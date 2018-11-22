f140416fc2c0a3ab6e39fcf9a28ae40c6842af67
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2018-11-22 22:05:36 +0530

    uploaded new source files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from './Components/NavBar';
+import Body from './Components/Body'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <Body/>
+      </div>
+    );
+  }
+}
+
+export default App;
