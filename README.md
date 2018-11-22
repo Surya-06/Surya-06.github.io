b2f3cc50c1a66a3e97a7589def1410129b9903a6
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2018-11-22 23:16:52 +0530

    uploading build files without gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# Surya-06.github.io
-My personal website
---
+++ precache-manifest.af87495f6e55a60302b00a4eb118cd2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9dee3b43877945dadc7",
+    "url": "/static/js/main.c9dee3b4.chunk.js"
+  },
+  {
+    "revision": "34f2730e6d91e3990332",
+    "url": "/static/js/1.34f2730e.chunk.js"
+  },
+  {
+    "revision": "c9dee3b43877945dadc7",
+    "url": "/static/css/main.9eda0d4f.chunk.css"
+  },
+  {
+    "revision": "f2decbf9eea1d3b0ba09af9b5905662e",
+    "url": "/index.html"
+  }
+];
